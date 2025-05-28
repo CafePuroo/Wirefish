9f1bcdc0893c3a06369233cab21bd36b40f12af4
CafePuroo <145449970+CafePuroo@users.noreply.github.com>
2025-05-28 19:38:20 -0300

    Create monitor_pacotes.sql

---
+++ monitor_pacotes.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS monitor_pacotes;
+USE monitor_pacotes;
+
+CREATE TABLE IF NOT EXISTS pacotes (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  src_ip VARCHAR(15) NOT NULL,
+  dest_ip VARCHAR(15) NOT NULL,
+  protocolo VARCHAR(10) NOT NULL,
+  tamanho INT NOT NULL
+);
