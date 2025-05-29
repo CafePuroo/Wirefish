2985023b170902d8fc1c6e7010cb83df5e66c973
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-05-29 14:30:01 -0300

    Update monitor_pacotes.sql

--- src/sql/monitor_pacotes.sql
+++ src/sql/monitor_pacotes.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE IF NOT EXISTS monitor_pacotes;
+CREATE DATABASE monitor_pacotes;
 USE monitor_pacotes;
 
-CREATE TABLE IF NOT EXISTS pacotes (
+CREATE TABLE IF NOT EXISTS PACOTES (
   id INT AUTO_INCREMENT PRIMARY KEY,
   src_ip VARCHAR(15) NOT NULL,
   dest_ip VARCHAR(15) NOT NULL,
@@ -9,11 +9,37 @@ CREATE TABLE IF NOT EXISTS pacotes (
   tamanho INT NOT NULL
 );
 
-CREATE TABLE usuarios (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  usuario VARCHAR(50) NOT NULL UNIQUE,
-  senha VARCHAR(255) NOT NULL
+CREATE TABLE USUARIOS (
+  id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(100),
+  email VARCHAR(100) UNIQUE,
+  senha_hash VARCHAR(255)
 );
+
+CREATE TABLE CAPTURA (
+    id_captura INT AUTO_INCREMENT PRIMARY KEY,
+    data_hora_inicio DATETIME,
+    interface VARCHAR(50),
+    id_usuario INT,
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
+);
+
+CREATE TABLE FILTRO (
+    id_filtro INT AUTO_INCREMENT PRIMARY KEY,
+    expressao TEXT,
+    id_captura INT,
+    FOREIGN KEY (id_captura) REFERENCES Captura(id_captura)
+);
+
+CREATE TABLE RELATORIO (
+    id_relatorio INT AUTO_INCREMENT PRIMARY KEY,
+    nome_arquivo VARCHAR(100),
+    data_geracao DATETIME,
+    tipo VARCHAR(50),
+    id_captura INT,
+    FOREIGN KEY (id_captura) REFERENCES Captura(id_captura)
+);
+
 
 -- Inserindo um usuário de teste (senha: 1234)
 INSERT INTO usuarios (usuario, senha)
