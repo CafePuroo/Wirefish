9eec7ec4bd59b35a933d69d757f003e1f219ab31
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-05-29 08:45:09 -0300

    Refatorando o diretório

--- backend/sql/criar_tabela.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE usuarios (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  usuario VARCHAR(50) NOT NULL UNIQUE,
-  senha VARCHAR(255) NOT NULL
-);
-
--- Inserindo um usuário de teste (senha: 1234)
-INSERT INTO usuarios (usuario, senha)
-VALUES ('admin', SHA2('1234', 256));
--- src/sql/monitor_pacotes.sql
+++ src/sql/monitor_pacotes.sql
@@ -8,3 +8,14 @@ CREATE TABLE IF NOT EXISTS pacotes (
   protocolo VARCHAR(10) NOT NULL,
   tamanho INT NOT NULL
 );
+
+CREATE TABLE usuarios (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  usuario VARCHAR(50) NOT NULL UNIQUE,
+  senha VARCHAR(255) NOT NULL
+);
+
+
+-- Inserindo um usuário de teste (senha: 1234)
+INSERT INTO usuarios (usuario, senha)
+VALUES ('admin', SHA2('1234', 256));
