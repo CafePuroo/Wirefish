58bbb3777a4deb55d17f3a7acd70ecdf5d1d07fc
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-05-29 14:29:55 -0300

    Create install.sh

---
+++ resources/install.sh
@@ -0,0 +1,9 @@
+#@Authores: Márcio Moda, João Manoel, Gabriel Pereira, Patrick Matos e Brener Picanço
+#
+#Arquivo para instalação de pacotes em sh
+#
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install tshark default-mysql-server default-mysql-client apache -y
