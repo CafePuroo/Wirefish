d081c5f622ed6c4bde048c1613d2c8c40041481f
Márcio Moda <69868170+TechnoDark-ti@users.noreply.github.com>
2025-06-02 13:52:25 -0300

    Update capturar.sh

--- resources/capturar.sh
+++ resources/capturar.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Interface de rede (pode ser substituída por argumento)
-INTERFACE=${1:-eth0}
-DURACAO=${2:-10}  # tempo em segundos
+chmod +x resources/capturar.sh
 
-# Comando tshark
+INTERFACE=${1:-eth0}
+DURACAO=${2:-10}
+
 tshark -i "$INTERFACE" -a duration:"$DURACAO" -T fields \
   -e ip.src -e ip.dst -e _ws.col.Protocol -e frame.len \
   -E separator=, -E quote=d -E header=n \
-  | grep -v '^""'  # Remove linhas vazias
+  | grep -v '^""'
